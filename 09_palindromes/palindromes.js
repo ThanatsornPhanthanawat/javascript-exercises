37086e6436b91a087df99ac5c3bc3de5db7850a4
ThanatsornPhanthanawat <pthanatsorn@gmail.com>
2025-03-18 17:32:27 +0700

    Practice function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
 const palindromes = function () {
+// Remove non-alphanumeric characters and convert to lowercase
+  const cleanedStr = str.replace(/[^a-z0-9]/gi, '').toLowerCase();
 
+  // Check if the string is equal to its reverse
+  const reversedStr = cleanedStr.split('').reverse().join('');
+  
+  return cleanedStr === reversedStr;
 };
 
 // Do not edit below this line
