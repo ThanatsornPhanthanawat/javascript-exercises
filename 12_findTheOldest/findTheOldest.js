3bae2e501b72106d1a6a4d244a2faa352c927be1
ThanatsornPhanthanawat <pthanatsorn@gmail.com>
2025-03-18 17:39:40 +0700

    Practice funtion

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
-};
-
-// Do not edit below this line
-module.exports = findTheOldest;
+const findTheOldest = function(people) {
+    return people.reduce((oldest, currentPerson) => {
+      // Calculate current person's age based on their birth and death year (if applicable)
+      const currentYear = new Date().getFullYear();
+      const age = (currentPerson.death || currentYear) - currentPerson.yearOfBirth;
+  
+      // Compare with the current oldest person
+      if (!oldest || age > oldest.age) {
+        return { ...currentPerson, age };
+      }
+      return oldest;
+    }, null);
+  };
+  
+  // Do not edit below this line
+  module.exports = findTheOldest;
+  
